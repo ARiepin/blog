a36fa3193dd7c6a1887b8b09fd353133c9165057
Andrey Repin <aarepin@gmail.com>
2014-04-06 19:54:25 +0300

    modified for devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Blog::Application.routes.draw do
   devise_for :users
   resources :posts
 
-  resources :users
+  #resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
